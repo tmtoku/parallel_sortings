daa18ea82eb674536fdb5998a14cf365bcb4d9d5
Tomoyuki Tokuue <80094371+qtmm@users.noreply.github.com>
2023-04-06 15:23:21 +0900

    Correct the use of numactl

--- jobs8/time.sh
+++ jobs8/time.sh
@@ -20,9 +20,10 @@ count=20
 # "sequential algo name", "parallel algo number", "num threads"
 test(){
     i=$2
-    export OMP_NUM_THREADS=$3
-    fipp -C -d profile -Icall,cpupa -m 10000 ./${1}.out ${N} ${count} ${i}>> "result/${algo[i]}_${1}${p}.time" 2>> "result/${algo[i]}_${1}${p}.err"
-    fipppx -A -pall -Ibalance,call -d profile -o "result/${algo[i]}_${1}${p}_${3}.prof"
+    th=$3
+    export OMP_NUM_THREADS=${th}
+    numactl -C 12-$((11+th)) fipp -C -d profile -Icall,cpupa -m 10000 ./${1}.out ${N} ${count} ${i}>> "result/${algo[i]}_${1}${p}.time" 2>> "result/${algo[i]}_${1}${p}.err"
+    fipppx -A -pall -Ibalance,call -d profile -o "result/${algo[i]}_${1}${p}_${th}.prof"
     rm -rf profile
 }
 
@@ -33,7 +34,6 @@ test 'pdq' 0 1
 for th in 1 2 4 12 24 48
 do
     export XOS_MMM_L_PAGING_POLICY=demand:demand:demand
-    numactl -C 12-$((11+th)) -m 4-$((4+(th-1)/12))
 
     test 'blockq' 1 ${th}
 
